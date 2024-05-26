a0920cd01c1e9ad5ab4b1ba274f0165a18e74a22
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2024-05-26 17:43:29 +0300

    c17

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -23,7 +23,7 @@ export default function Search() {
       area: area,
       floor: floor,
       district: district,
-      imgUrl:'/p-1.png'
+      imgUrl:"/p-1.png"
     };
 
     // Make the fetch request
